573e525096c3908d7b8fa139b8a9246378858c62
João Marco <moaojarco@gmail.com>
2022-06-16 01:27:03 -0400

    fix idiot mistake

--- config/nvim/nvim/lua/plugin-config/nvim-treesitter.lua
+++
@@ -1,8 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  -- will install treesitter for all available languages
-  ensure_installed = 'all',
-  ignore_install = {"haskell"}, -- broken
-  highlight = {
-    enable = true
-  }
-}
--- config/nvim/nvim/lua/post-plugin-basics.lua
+++
@@ -1,7 +0,0 @@
--- ################# Basic settings dependent on plugins ################ --
-
--- ================= Visualization ================= --
-
-vim.o.termguicolors = true
-vim.o.background = 'dark'
-vim.cmd('colorscheme tokyonight')
