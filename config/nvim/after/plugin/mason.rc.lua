6a741f9122b80c6ffd6681bfdd3474d41715a91d
João Marco <moaojarco@gmail.com>
2022-12-02 16:09:09 -0300

    mason: add rust_analyzer, html

--- config/nvim/after/plugin/mason.rc.lua
+++ config/nvim/after/plugin/mason.rc.lua
@@ -4,9 +4,8 @@ local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
 mason.setup({
-
 })
 
 lspconfig.setup {
-  ensure_installed = { "sumneko_lua", "tailwindcss", "gopls", "omnisharp" },
+  ensure_installed = { "sumneko_lua", "tailwindcss", "gopls", "omnisharp", "rust_analyzer", "html" },
 }
