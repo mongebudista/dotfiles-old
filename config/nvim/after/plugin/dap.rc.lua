c930249d83fbfc94a35ca5b0a8983a65860f9e89
João Marco <moaojarco@gmail.com>
2022-12-02 16:08:15 -0300

    feat:setup debugger (dap&dapui)

---
+++ config/nvim/after/plugin/dap.rc.lua
@@ -0,0 +1,19 @@
+local status, dap = pcall(require, "dap")
+if (not status) then return end
+
+dap.adapters.coreclr = {
+  type = 'executable',
+  command = '/usr/bin/netcoredbg',
+  args = {'--interpreter=vscode'}
+}
+
+dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/net7.0/', 'file')
+    end,
+  },
+}
